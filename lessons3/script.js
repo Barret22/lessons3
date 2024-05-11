aa3c59c6d22003abfb8a33db70e9d141727eb7ba
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-05-11 16:44:23 +0200

    DZ 3.1

---
+++ lessons3/script.js
@@ -0,0 +1,35 @@
+
+let orang = 10;
+let apple = 20;
+const MAX_VALUE = 500;
+console.log(orang + ' ' + apple + ' ' + MAX_VALUE)
+
+
+const max_orang = 12347937568345n;
+console.log(max_orang)
+
+
+let welcom = "Hello Viktor"
+console.log(welcom)
+alert(welcom)
+
+
+let itissunnytoday = true;
+let itisnotsunnytoday= false;
+console.log(itisnotsunnytoday)
+
+
+let banan = null ;
+console.log(banan)
+
+
+let  citrus = undefined;
+console.log(citrus)
+
+
+let uzer = {
+    name:"Viktor",
+    age:42
+};
+console.log(uzer.name)
+console.log(uzer.age)
